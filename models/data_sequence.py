d532d8706ac6cd5c2ec330cb37e114c6ef2dddb0
logicvay2010 <logic_hui@163.com>
2023-09-26 18:56:10 -0700

    model folder upload

---
+++ models/data_sequence.py
@@ -0,0 +1,147 @@
+import os
+import numpy as np
+import torch
+from torch.utils.data.dataset import Dataset
+import mrcfile
+from TomoNet.preprocessing.img_processing import normalize
+
+class Train_sets(Dataset):
+    def __init__(self, data_dir, prefix = "train"):
+        super(Train_sets, self).__init__()
+        self.path_all = []
+        for d in  [prefix+"_x", prefix+"_y"]:
+            p = '{}/{}/'.format(data_dir, d)
+            self.path_all.append(sorted([p+f for f in os.listdir(p)]))
+        #print(self.path_all)
+
+    def __getitem__(self, idx):
+        with mrcfile.open(self.path_all[0][idx]) as mrc:
+            #print(self.path_all[0][idx])
+            rx = mrc.data[np.newaxis,:,:,:]
+            # rx = mrc.data[:,:,:,np.newaxis]
+        with mrcfile.open(self.path_all[1][idx]) as mrc:
+            #print(self.path_all[1][idx])
+            ry = mrc.data[np.newaxis,:,:,:]
+            # ry = mrc.data[:,:,:,np.newaxis]
+        rx = torch.from_numpy(rx.copy())
+        ry = torch.from_numpy(ry.copy())
+        return rx, ry
+
+    def __len__(self):
+        return len(self.path_all[0])
+
+class Train_sets_new(Dataset):
+    def __init__(self, data_dir, prefix = "train"):
+        super(Train_sets_new, self).__init__()
+        self.path_all = []
+        for d in  [prefix+"_x", prefix+"_y"]:
+            p = '{}/{}/'.format(data_dir, d)
+            self.path_all.append(sorted([p+f for f in os.listdir(p)]))
+        #print(self.path_all)
+
+    def __getitem__(self, idx):
+        with mrcfile.open(self.path_all[0][idx]) as mrc:
+            #print(self.path_all[0][idx])
+            rx = mrc.data[np.newaxis,:,:,:]
+            # rx = mrc.data[:,:,:,np.newaxis]
+        with mrcfile.open(self.path_all[1][idx]) as mrc:
+            #print(self.path_all[1][idx])
+            ry = mrc.data[np.newaxis,:,:,:]
+            # ry = mrc.data[:,:,:,np.newaxis]
+        rx = torch.from_numpy(rx.copy())
+        ry = torch.from_numpy(ry.copy())
+        #ry1 = torch.from_numpy(ry.copy())
+        #ry2 = torch.from_numpy(1-ry.copy())
+        #ry = np.concatenate((ry1,ry2))
+        #ry = ry.reshape(tuple([2])+ry1.shape[1:])
+        #print(ry.shape)
+        return rx, ry
+
+    def __len__(self):
+        return len(self.path_all[0])
+
+class Predict_sets(Dataset):
+    def __init__(self, mrc_list, inverted=True):
+        super(Predict_sets, self).__init__()
+        self.mrc_list=mrc_list
+        self.inverted = inverted
+
+    def __getitem__(self, idx):
+        with mrcfile.open(self.mrc_list[idx]) as mrc:
+            rx = mrc.data[np.newaxis,:,:,:].copy()
+        # rx = mrcfile.open(self.mrc_list[idx]).data[:,:,:,np.newaxis]
+        if self.inverted:
+            rx=normalize(-rx, percentile = True)
+
+        return rx
+
+    def __len__(self):
+        return len(self.mrc_list)
+
+
+
+def get_datasets(data_dir):
+    train_dataset = Train_sets_new(data_dir, prefix="train")
+    val_dataset = Train_sets_new(data_dir, prefix="test")
+    return train_dataset, val_dataset#, bench_dataset
+
+
+class Train_sets_angle(Dataset):
+    def __init__(self, data_dir, prefix = "train"):
+        super(Train_sets_angle, self).__init__()
+        self.path_all = []
+        for d in  [prefix+"_x", prefix+"_y"]:
+            p = '{}/{}/'.format(data_dir, d)
+            self.path_all.append(sorted([p+f for f in os.listdir(p)]))
+        #print(self.path_all)
+
+    def __getitem__(self, idx):
+        with mrcfile.open(self.path_all[0][idx]) as mrc:
+            #print(self.path_all[0][idx])
+            rx = mrc.data[np.newaxis,:,:,:]
+            # rx = mrc.data[:,:,:,np.newaxis]
+        with mrcfile.open(self.path_all[1][idx]) as mrc:
+            #print(self.path_all[1][idx])
+            ry = mrc.data[np.newaxis,:,:,:]
+            # ry = mrc.data[:,:,:,np.newaxis]
+        rx_pre = torch.from_numpy(rx.copy())
+        rx_shape = rx_pre.shape
+        rx = rx_pre.reshape((2,rx_shape[1]//2,rx_shape[2],rx_shape[3]))
+        ry_pre = torch.from_numpy(ry.copy())
+        ry_shape = ry_pre.shape
+        ry = ry_pre.reshape((3,ry_shape[1]//3,ry_shape[2],ry_shape[3]))
+        #ry1 = torch.from_numpy(ry.copy())
+        #ry2 = torch.from_numpy(1-ry.copy())
+        #ry = np.concatenate((ry1,ry2))
+        #ry = ry.reshape(tuple([2])+ry1.shape[1:])
+        #print(ry.shape)
+        return rx, ry
+
+    def __len__(self):
+        return len(self.path_all[0])
+    
+def get_datasets_angles(data_dir):
+    train_dataset = Train_sets_angle(data_dir, prefix="train")
+    val_dataset = Train_sets_angle(data_dir, prefix="test")
+    return train_dataset, val_dataset#, bench_dataset
+
+
+class Predict_sets_angle(Dataset):
+    def __init__(self, mrc_list, inverted=True):
+        super(Predict_sets_angle, self).__init__()
+        self.mrc_list=mrc_list
+        self.inverted = inverted
+
+    def __getitem__(self, idx):
+        with mrcfile.open(self.mrc_list[idx]) as mrc:
+            rx = mrc.data[np.newaxis,:,:,:].copy()
+        # rx = mrcfile.open(self.mrc_list[idx]).data[:,:,:,np.newaxis]
+        if self.inverted:
+            rx=normalize(-rx, percentile = True)
+        sp = rx.shape
+        rx = rx.reshape(2,sp[1]//2,sp[2],sp[3])
+        #print(rx.shape)
+        return rx
+
+    def __len__(self):
+        return len(self.mrc_list)
---
+++ models/unet.py
@@ -0,0 +1,182 @@
+from typing import List
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+class ConvBlock(pl.LightningModule):
+    # conv_per_depth fixed to 2
+    def __init__(self, in_channels, out_channels, n_conv, kernel_size =3, stride=1, padding=1):
+        super(ConvBlock, self).__init__()
+        layers = [
+            nn.Conv3d(in_channels=in_channels, out_channels=out_channels,
+                    kernel_size=kernel_size, stride=stride, padding=padding, bias=True), 
+            #nn.InstanceNorm3d(num_features = out_channels),
+            nn.BatchNorm3d(num_features=out_channels),
+            nn.LeakyReLU(),
+        ]
+        for _ in range(max(n_conv-1,0)):
+            layers.append(nn.Conv3d(in_channels=out_channels, out_channels=out_channels,
+                    kernel_size=kernel_size, stride=stride, padding=padding, bias=True))
+            #layers.append(nn.InstanceNorm3d(num_features=out_channels))
+            layers.append(nn.BatchNorm3d(num_features=out_channels))
+            layers.append(nn.LeakyReLU())
+
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x)
+
+class EncoderBlock(pl.LightningModule):
+    def __init__(self, filter_base, unet_depth, n_conv):
+        super(EncoderBlock, self).__init__()
+        self.module_dict = nn.ModuleDict()
+        self.module_dict['first_conv'] = nn.Conv3d(in_channels=1, out_channels=filter_base[0], kernel_size=3, stride=1, padding=1)
+
+        for n in range(unet_depth):
+            self.module_dict["conv_stack_{}".format(n)] = ConvBlock(in_channels=filter_base[n], out_channels=filter_base[n], n_conv=n_conv)
+            self.module_dict["stride_conv_{}".format(n)] = ConvBlock(in_channels=filter_base[n], out_channels=filter_base[n+1], n_conv=1, kernel_size=2, stride=2, padding=0)
+        
+        self.module_dict["bottleneck"] = ConvBlock(in_channels=filter_base[n+1], out_channels=filter_base[n+1], n_conv=n_conv-1)
+    
+    def forward(self, x):
+        down_sampling_features = []
+        for k, op in self.module_dict.items():
+            x = op(x)
+            if k.startswith('conv'):
+                down_sampling_features.append(x)
+        return x, down_sampling_features
+
+class DecoderBlock(pl.LightningModule):
+    def __init__(self, filter_base, unet_depth, n_conv):
+        super(DecoderBlock, self).__init__()
+        self.module_dict = nn.ModuleDict()
+        for n in reversed(range(unet_depth)):
+            self.module_dict["deconv_{}".format(n)] = nn.ConvTranspose3d(in_channels=filter_base[n+1],
+                                                                         out_channels=filter_base[n],
+                                                                         kernel_size=2,
+                                                                         stride=2,
+                                                                         padding=0)
+            self.module_dict["activation_{}".format(n)] = nn.LeakyReLU()
+            self.module_dict["conv_stack_{}".format(n)] = ConvBlock(filter_base[n]*2, filter_base[n],n_conv=n_conv)
+        
+    def forward(self, x,
+        down_sampling_features: List[torch.Tensor]):
+        for k, op in self.module_dict.items():
+            x=op(x)
+            if k.startswith("deconv"):
+                x = torch.cat((down_sampling_features[int(k[-1])], x), dim=1)
+        return x
+
+class Unet(pl.LightningModule):
+    def __init__(self,filter_base = 64, out_channels=1, learning_rate = 3e-4, add_last=False, metrics=None):
+        super(Unet, self).__init__()
+        self.add_last = add_last
+        if filter_base == 64:
+            filter_base = [64,128,256,320,320,320]
+        elif filter_base == 32:
+            filter_base = [32,64,128,256,320,320]
+        elif filter_base == 16:
+            filter_base = [16,32,64,128,256,320]
+        #filter_base = [1,1,1,1,1]
+        unet_depth = 3
+        n_conv = 3
+        self.encoder = EncoderBlock(filter_base=filter_base, unet_depth=unet_depth, n_conv=n_conv)
+        self.decoder = DecoderBlock(filter_base=filter_base, unet_depth=unet_depth, n_conv=n_conv)
+        self.final = nn.Conv3d(in_channels=filter_base[0], out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.variance_out = False
+        if self.variance_out:
+            self.mse_layer = nn.Sequential(
+                nn.Conv3d(in_channels=filter_base[0], out_channels=filter_base[0]//2, kernel_size=3, stride=1, padding=1),
+                nn.LeakyReLU(),
+                nn.Conv3d(in_channels=filter_base[0]//2, out_channels=filter_base[0]//4, kernel_size=3, stride=1, padding=1),
+                nn.LeakyReLU(),
+                nn.Conv3d(in_channels=filter_base[0]//4, out_channels=filter_base[0]//8, kernel_size=3, stride=1, padding=1),
+                nn.LeakyReLU(),
+                nn.Conv3d(in_channels=filter_base[0]//8, out_channels=1, kernel_size=1, stride=1, padding=0),
+                nn.Softplus()
+            )
+        
+
+        self.learning_rate = learning_rate
+        if metrics is None:
+            self.metrics = {'train_loss':[], 'val_loss':[]}
+        else:
+            self.metrics = metrics
+    
+    def forward(self, x):
+        x_org = x
+        if self.variance_out:
+            with torch.no_grad():
+                x, down_sampling_features = self.encoder(x)
+                x = self.decoder(x, down_sampling_features)
+                y_hat = self.final(x)#  + x_org
+            mse_map = self.mse_layer(x) + 10**-3
+            return [y_hat,mse_map]
+        else:
+            x, down_sampling_features = self.encoder(x)
+            x = self.decoder(x, down_sampling_features)
+            y_hat = self.final(x)
+            if self.add_last:
+                y_hat += x_org
+            return y_hat
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        out = self(x)
+        #print(out.shape)
+        #print(y.shape)
+        #print(self.variance_out)
+        if self.variance_out:
+            #loss = nn.L1Loss()(out[1], torch.abs(out[0]-y))
+            c = 0.6931471805599453 # log(2)
+            loss = torch.mean(torch.div(torch.abs(out[0]-y), out[1]) + torch.log(out[1])) + c
+        else:
+            #loss = nn.L1Loss()(out, y)
+            #loss = nn.MSELoss()(out, y)
+            #loss = nn.CrossEntropyLoss()(out, y)
+            loss = nn.BCEWithLogitsLoss()(out, y)
+
+        return loss
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate)
+        return optimizer 
+
+    def validation_step(self, batch, batch_idx):
+        with torch.no_grad():
+            x, y = batch
+            out = self(x)
+            if self.variance_out:
+                #loss = nn.L1Loss()(out[1], torch.abs(out[0]-y))
+                c = 0.6931471805599453 # log(2)
+                loss = torch.mean(torch.div(torch.abs(out[0]-y), out[1]) + torch.log(out[1])) + c
+            else:
+                #loss = nn.L1Loss()(out, y)
+                #loss = nn.MSELoss()(out, y)
+                #loss = nn.CrossEntropyLoss()(out, y)
+                loss = nn.BCEWithLogitsLoss()(out, y)
+
+            return loss
+    
+    def training_epoch_end(self, outputs):
+        #print(outputs)
+        loss = torch.stack([x['loss'] for x in outputs]).mean().item()
+        self.metrics["train_loss"].append(loss)
+        #self.log("train_loss", loss, logger=True,on_epoch=True)
+
+    
+
+    def validation_epoch_end(self, outputs):
+        loss = torch.stack(outputs).mean().item()
+        self.metrics["val_loss"].append(loss)
+        self.log("val_loss", loss, prog_bar=True, on_epoch=True)
+    '''
+    def on_training_epoch_end(self, outputs):
+        loss = torch.stack([x['loss'] for x in outputs]).mean().item()
+        self.metrics["train_loss"].append(loss)
+        #self.log("train_loss", loss, logger=True,on_epoch=True)
+    def on_validation_epoch_end(self, outputs):
+        loss = torch.stack(outputs).mean().item()
+        self.metrics["val_loss"].append(loss)
+        self.log("val_loss", loss, prog_bar=True,on_epoch=True)
+    '''
